14826b105250158e2b11f9e5196e550af64f3a53
RJ <871949131@qq.com>
2018-02-07 15:57:24 +0800

    change

--- config.js
+++ config.js
@@ -9,4 +9,450 @@ Date:2018年2月5日
 	"appsrcret": "xxxxxxxxxxxxxxxxxxxxxxxxxx",
 }
 */
-export default []
+export default [
+	{
+		"name": "幻爱小说",
+		"appid": "wx8e18143947eedfea",
+		"appsrcret": "5bbd5c9b01244e3d5d74ee732d1cb716",
+	},
+    {
+        "name": "情伊小说",
+        "appid": "wxd85744ed248c9b45",
+        "appsrcret": "c519863e940dc7f11c68dd412042f7a0",
+    },
+    {
+        "name": "猎艳书斋",
+        "appid": "wxbc491e6db1df9d07",
+        "appsrcret": "ae4ef4537c3a57324529aadc4331e32c",
+    },
+    {
+        "name": "香艳书殿",
+        "appid": "wx0d6a812c1f6408e2",
+        "appsrcret": "98f44e298170d22e6658fef943cec9c3",
+    },
+    {
+        "name": "情梦小说",
+        "appid": "wx51bcbcdf775a7d6e",
+        "appsrcret": "2716c1d124402503da668cf835c35dc8",
+    },
+    {
+        "name": "火辣书殿",
+        "appid": "wx30cc5aa8c697e03b",
+        "appsrcret": "72d519bca25b8041fe8a89789d2b70ef",
+    },
+    {
+        "name": "恋香书房",
+        "appid": "wxeeb37916a8da06eb",
+        "appsrcret": "ebb96c77c077be3a7111a614f0c48253",
+    },
+    {
+        "name": "欢情小说",
+        "appid": "wx368f8a87a3f46125",
+        "appsrcret": "354a7e4ea30ef4cbe0a6bc28b4022795",
+    },
+    {
+        "name": "情欢小说",
+        "appid": "wx1427906616a6557a",
+        "appsrcret": "51a2495ad9a2974c8ba33f5d094d20b2",
+    },
+    {
+        "name": "情迷书殿",
+        "appid": "wx679844ef105eebea",
+        "appsrcret": "bfdcd3b8bc07d9e7b12ffb0797a14ffe",
+    },
+    {
+        "name": "美艳书榜",
+        "appid": "wx5f82c2d4e2569cdf",
+        "appsrcret": "f266b72cfca20a87e0c71fc293f059d8",
+    },
+    {
+        "name": "迷人书城",
+        "appid": "wx96327dc79e081b26",
+        "appsrcret": "744d778cd02967294c7903de626035b6",
+    },
+    {
+        "name": "美艳书殿",
+        "appid": "wx057184486a08f39b",
+        "appsrcret": "9d69369b4b5dc3d05e78fa3c95e7323d",
+    },
+    {
+        "name": "香艳书榜",
+        "appid": "wx9a9f7bcd8ba0a19d",
+        "appsrcret": "a29d7331cdc527691ddc98620148a03d",
+    },
+    {
+        "name": "欲情书城",
+        "appid": "wxefeb2a6c6c0cda7e",
+        "appsrcret": "9a5bca2f4f643b7ec91f0010b44b48a1",
+    },
+    {
+        "name": "情爱书房",
+        "appid": "wx9591564369d99a4e",
+        "appsrcret": "4ca9cf80bb62c3919bf4665e53a5a388",
+    },
+    {
+        "name": "艳艳书房",
+        "appid": "wxeed5683d518264cb",
+        "appsrcret": "db94d02a90242f888a9909577629b6ce",
+    },
+    {
+        "name": "最美书殿",
+        "appid": "wx59efaf19ae78f6ba",
+        "appsrcret": "260e0c558450c9bf9a2763fbe2e4ff8c",
+    },
+    {
+        "name": "火辣书屋",
+        "appid": "wxaff4619d5bfe0c08",
+        "appsrcret": "b95b68a1ff2210396c384e2b2385d97e",
+    },
+    {
+        "name": "绝美书城",
+        "appid": "wxbe801c5d02a2f82c",
+        "appsrcret": "326d66f7d9ffde74969f22058235e03a",
+    },
+    {
+        "name": "春梦书阁",
+        "appid": "wx234bd3d3b426d7f2",
+        "appsrcret": "282d525d63b23d2812568d17d8c5ef86",
+    },
+    {
+        "name": "春梦书殿",
+        "appid": "wx40e4a2a3896a9ba1",
+        "appsrcret": "3316082f7bc2e93188a334fb630ccc58",
+    },
+    {
+        "name": "激欢书房",
+        "appid": "wxcd4247de5692e34f",
+        "appsrcret": "b807a82b48c56734823c38bbc2529f90",
+    },
+    {
+        "name": "刺激书屋",
+        "appid": "wxf4ce7362e340d9f7",
+        "appsrcret": "7a3566a9fdc107b62d619d3caac59ca1",
+    },
+    {
+        "name": "春情书殿",
+        "appid": "wx50310e745ff8cfd3",
+        "appsrcret": "eab4a9b80def48db69e714fc75450445",
+    },
+    {
+        "name": "欢愉书房",
+        "appid": "wxa4a0cbbcbe6dab61",
+        "appsrcret": "1b1cf1036a337934c8add102dd842735",
+    },
+    {
+        "name": "欢梦小说",
+        "appid": "wx34422e0f319db3c3",
+        "appsrcret": "2686763ab8d11dc0d3714740b42cf6ad",
+    },
+    {
+        "name": "玉女书房",
+        "appid": "wx2d68380e9d1bb936",
+        "appsrcret": "3e91d4f251253b5ce1b232086bfc70d2",
+    },
+    {
+        "name": "情深深小说",
+        "appid": "wxba894c48ab9fb3b9",
+        "appsrcret": "4ffa81ef475da331fea8a0cd12923261",
+    },
+    {
+        "name": "美梦小说",
+        "appid": "wxe42023eaa7c55147",
+        "appsrcret": "bf9be6547aa11bfef768e1816098dc8a",
+    },
+    {
+        "name": "宝妈小说",
+        "appid": "wx73334bbc0efeafe4",
+        "appsrcret": "6f7fc368f08c65a7f8d5c0a055489834",
+    },
+    {
+        "name": "粉红书院",
+        "appid": "wx3d9b4e15d14fd45e",
+        "appsrcret": "e597c2873507ba46aa5df4db1e7d98e3",
+    },
+    {
+        "name": "浪荡书苑",
+        "appid": "wxd13ea623b15b720f",
+        "appsrcret": "92250d444f30d303c3bbff9ba138a6f2",
+    },
+    {
+        "name": "樱花书房",
+        "appid": "wxc77045b61599ace7",
+        "appsrcret": "726adedb9364bcb36447192ecfbba73f",
+    },
+    {
+        "name": "微微书苑",
+        "appid": "wxc3d008209887025d",
+        "appsrcret": "88ff7a20443882bb4cbb3fb49aac23ea",
+    },
+    {
+        "name": "萌爱书诚",
+        "appid": "wx38428991861167e0",
+        "appsrcret": "b99d4b49c08b85f8eb1f0bfd53090403",
+    },
+    {
+        "name": "梦幻书诚",
+        "appid": "wxdd5b24bbfd3367f6",
+        "appsrcret": "343b207367480ca615eb97c4b040152d",
+    },
+    {
+        "name": "星梦书城",
+        "appid": "wx3de0e7cf7487cced",
+        "appsrcret": "cf5eaaf74ff770df03268cbb1e395136",
+    },
+    {
+        "name": "梦幻书房",
+        "appid": "wxa9cd7177962083db",
+        "appsrcret": "d7b384349f94a77dfca3f165b81e75d7",
+    },
+    {
+        "name": "星梦书阁",
+        "appid": "wxa770d8d7bceb0d6e",
+        "appsrcret": "a3f092c858a265629f3ec114e084e42a",
+    },
+    {
+        "name": "幻爱书城",
+        "appid": "wxfc111fbf14a021c9",
+        "appsrcret": "f852e2bf9bc5c3876291b17efc73a246",
+    },
+    {
+        "name": "触心阅读",
+        "appid": "wxb28ca6376c5fc312",
+        "appsrcret": "58cc8db562c2a2c258554b560d3c8d68",
+    },
+    {
+        "name": "心欢书屋",
+        "appid": "wx4768314aa86ad5cd",
+        "appsrcret": "08487c3548eb7581ad1675d8e6d3ce00",
+    },
+    {
+        "name": "舒君书殿",
+        "appid": "wxa25c7d0670adc4c9",
+        "appsrcret": "ae0e2822d745d508d0432962a6ec5081",
+    },
+    {
+        "name": "悦文书殿",
+        "appid": "wx0a40578d6967849b",
+        "appsrcret": "024f5b5695bfabb69068222083ad0a54",
+    },
+    {
+        "name": "娱阅书城",
+        "appid": "wx9c5c217f38ff277e",
+        "appsrcret": "479878bc3aa638ba8c29e8c57d084909",
+    },
+    {
+        "name": "舒君书斋",
+        "appid": "wxc90820400c6640ba",
+        "appsrcret": "d45eb8501ab3871b4789684bebaa785a",
+    },
+    {
+        "name": "御龙书斋",
+        "appid": "wx58cdf171f2b6d5d0",
+        "appsrcret": "ec76eead9f250b02bd8180f5d659fe5a",
+    },
+    {
+        "name": "悦情书屋",
+        "appid": "wx52ef8487003ec7e6",
+        "appsrcret": "9fc6311e2e02d2689701552319110bfd",
+    },
+    {
+        "name": "醉香书城",
+        "appid": "wxad0c00844a07e441",
+        "appsrcret": "8513da1dd01c0eabb95d0db7bf5898a4",
+    },
+    {
+        "name": "情悦书房",
+        "appid": "wxd7d794423d13d7b2",
+        "appsrcret": "1fca40d30e52c30e579836d8d2fcaea8",
+    },
+    {
+        "name": "深夜书楼",
+        "appid": "wxe986639a1af785c5",
+        "appsrcret": "b63453bac3e9474184cff1acc14cc0c9",
+    },
+    {
+        "name": "倾城书斋",
+        "appid": "wx37c00320f3e9a399",
+        "appsrcret": "14dc344df071ded58004de56a3c50952",
+    },
+    {
+        "name": "梦爱书房",
+        "appid": "wxe30b4853a1cb8806",
+        "appsrcret": "6b5f2b1678d973436caff6f464f51c62",
+    },
+    {
+        "name": "幻夜书房",
+        "appid": "wx31fb640dbd4659b8",
+        "appsrcret": "0e6bb8edf2af752e6460de0bbd1abca8",
+    },
+    {
+        "name": "香阅书房",
+        "appid": "wxb10f3402edfc0e26",
+        "appsrcret": "eb5ee84a72d1c706b2fd560d83c0aebc",
+    },
+    {
+        "name": "欢欢书殿",
+        "appid": "wx5b6b1b5c3038978e",
+        "appsrcret": "f538ba7bf1d0832a63c2bb0f24e1c424",
+    },
+    {
+        "name": "欢阅书屋",
+        "appid": "wx2d6ff49a10f9880b",
+        "appsrcret": "f63c764d40f22de5f5c47c33b574836a",
+    },
+    {
+        "name": "恋香书斋",
+        "appid": "wxb93653b8f48facd5",
+        "appsrcret": "baf21e5316687ffe4f772a1d7d3467fa",
+    },
+    {
+        "name": "女人阅书",
+        "appid": "wx1b67d4f319dc0071",
+        "appsrcret": "7e7fc79e8c97d43ae7f7e921f6082c44",
+    },
+    {
+        "name": "丽人书舍",
+        "appid": "wx6f6795cde90979a4",
+        "appsrcret": "084b10dc8b3887d47bb09d63c4ab5950",
+    },
+    {
+        "name": "添情小说",
+        "appid": "wxec0ac68d97b64491",
+        "appsrcret": "068e45aa041f8a4d8b696ba17ab8432f",
+    },
+    {
+        "name": "百魅书殿",
+        "appid": "wxcc32ebc80cd3486a",
+        "appsrcret": "1d4c9024d48965de7ca50ab0f5559aab",
+    },
+    {
+        "name": "欲情书殿",
+        "appid": "wx86a682b6e86a0f73",
+        "appsrcret": "95ddeeb298055f8cce20bdee3917dc08",
+    },
+    {
+        "name": "含情书殿",
+        "appid": "wx5830eb7c5b3e3129",
+        "appsrcret": "c960d7ac80b677b31be04fc1718fb7c7",
+    },
+    {
+        "name": "夜半书阁",
+        "appid": "wx9e45b05c1a330a3a",
+        "appsrcret": "8cae82b53b7abec65d18859a3e1b7d03",
+    },
+    {
+        "name": "掌上阅书城",
+        "appid": "wxafdca971fe671915",
+        "appsrcret": "1afd1410c81063a75d94b618a6a90265",
+    },
+    {
+        "name": "欲心书屋",
+        "appid": "wxe167828b88bd4124",
+        "appsrcret": "1817b4564ccb8cf1130a2e27db068c85",
+    },
+    {
+        "name": "女神等你来",
+        "appid": "wxdf69657ae6fcd74d",
+        "appsrcret": "abc7b4df73106dc39035e37a91abc906",
+    },
+    {
+        "name": "丽人小书屋",
+        "appid": "wx4fd3c7922447c7ab",
+        "appsrcret": "e98c316ae504659ff4d62c2f1eca04a0",
+    },
+    {
+        "name": "幻梦书屋",
+        "appid": "wxb74f33a3767c4eb2",
+        "appsrcret": "03882cfa2ce4d6541240b40030bf4e6a",
+    },
+    {
+        "name": "女人阅书房",
+        "appid": "wxabfefe85c096c016",
+        "appsrcret": "1c3bbd5e97f2efd31c9bad9e4cb13a10",
+    },
+    {
+        "name": "心恋书城",
+        "appid": "wx0fecfa9b3cbd2ffb",
+        "appsrcret": "db368e7740ff115d000c11a89d22cb57",
+    },
+    {
+        "name": "春恋书阁",
+        "appid": "wxb1de6cfa7dc69bee",
+        "appsrcret": "95e4953973f5f2c0cca726c6fdbdb523",
+    },
+    {
+        "name": "悦君书斋",
+        "appid": "wxdfafaa7a45d99591",
+        "appsrcret": "004a149aaa28f591ec1c5c3bf557d2f3",
+    },
+    {
+        "name": "情梦书房",
+        "appid": "wxc4f2ff6e7946c60f",
+        "appsrcret": "aa9adad98f00965080e182e988c3bfe1",
+    },
+    {
+        "name": "倾情伴读",
+        "appid": "wx3d6d6423958c5947",
+        "appsrcret": "b6b1ebc1c5289d043b7e815b1fd40428",
+    },
+    {
+        "name": "舒心书房",
+        "appid": "wx0a31c77430a31e8d",
+        "appsrcret": "461c948741e2c54689fbcaa79512bb4d",
+    },
+    {
+        "name": "梦情书殿",
+        "appid": "wxde7e958b8dbb836c",
+        "appsrcret": "917d16d7c7e9c88392e5c559ca8da333",
+    },
+    {
+        "name": "芯梦书城",
+        "appid": "wx82ebef2f92fce23c",
+        "appsrcret": "558993852554723cce2e92cf18fd2bfb",
+    },
+    {
+        "name": "阅艳书城",
+        "appid": "wx1585684fd9704d9c",
+        "appsrcret": "f82f1dd1ba5574d49db89f72e8af545f",
+    },
+    {
+        "name": "欢娱书城",
+        "appid": "wx47c4d9d9f1e1d9da",
+        "appsrcret": "edd8bd49a631aca04422dc386430e7d1",
+    },
+    {
+        "name": "欢欲书城",
+        "appid": "wx6fccad2f0d886765",
+        "appsrcret": "ae618b7bc3d2b6d7b8b6396be7b5c980",
+    },
+    {
+        "name": "美恋书城",
+        "appid": "wxab77cf88f6210e21",
+        "appsrcret": "356f18663a8b02ef9be9d38a40e88077",
+    },
+    {
+        "name": "欢情书屋",
+        "appid": "wxf7f2c42376324707",
+        "appsrcret": "174e0e4eb56fb38770d589b7c4816835",
+    },
+    {
+        "name": "悦心书殿",
+        "appid": "wx909191c4cd06abc7",
+        "appsrcret": "6e518b24fdbc34db5d8778a90ef76994",
+    },
+    {
+        "name": "欢爱书殿",
+        "appid": "wx87693de4f60b82a1",
+        "appsrcret": "b1e4a042051c54149b1b92b1448ff85d",
+    },
+    {
+        "name": "欢芯书城",
+        "appid": "wxc735516eb07ff30f",
+        "appsrcret": "8c4cddf217db59dff76f6eeb7d67d0a2",
+    },
+    {
+        "name": "魅娇书城",
+        "appid": "wx5cf8c88158fd3599",
+        "appsrcret": "31273d57df51c88fe59566f193da56f4",
+    },
+]
--- wx_user.js
+++ wx_user.js
@@ -18,39 +18,40 @@ export default {
         for (let i = 0; i < config.length; i++) {
             let c = config[i];
 
-            // get access token
-            if (!c.access_token || c.expires < Date.now()) {
-                let at = null;
+            for(let z = 0; z < 3; z++) {
+                // get access token
+                if (!c.access_token || c.expires < Date.now()) {
+                    let at = null;
+
+                    try {
+                        at = await wx_api.access_token(c);
+                    } catch (e) {
+                        log(e);
+                    }
+
+                    if (at && at.access_token) {
+                        c.access_token = at.access_token;
+                        c.expires = Date.now() + (at.expires_in-60) * 1000;
+                    }
+                }
+
+                if (!c.access_token) {
+                    log(`\nERROR: ${c.name}获取access_token失败`);
+                    continue;
+                }
 
                 try {
-                    at = await wx_api.access_token(c);
-                } catch (e) {
-                    log(e);
+                    let usercumulate = await wx_api.getusercumulate(c, begin_date, end_date);
+                    let usersummary = await wx_api.getusersummary(c, begin_date, end_date);
+
+                    txt += "\n" + map.data(c, usercumulate, usersummary);
+                    break;
+                } catch(e) {
+                    if (e.errcode == 40001) {
+                        c.access_token = null;
+                    }
+                    log(JSON.stringify(e));
                 }
-
-                if (at && at.access_token) {
-                    c.access_token = at.access_token;
-                    c.expires = Date.now() + (at.expires_in-60) * 1000;
-                }
-            }
-
-            if (!c.access_token) {
-                log(`\nERROR: ${c.name}获取access_token失败`);
-                continue;
-            }
-
-            try {
-                let usercumulate = await wx_api.getusercumulate(c, begin_date, end_date);
-                let usersummary = await wx_api.getusersummary(c, begin_date, end_date);
-
-                console.log(usercumulate, usersummary)
-                txt += "\n" + map.data(c, usercumulate, usersummary);
-            } catch(e) {
-                if (e.errcode == 40001) {
-                    c.access_token = null;
-                }
-                console.log(begin_date, end_date)
-                log(JSON.stringify(e));
             }
         }
 
